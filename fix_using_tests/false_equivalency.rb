509553bc607ca65a552836dcdeeb1cec0979da63
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-24 18:07:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,12 +13,12 @@ end
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  else num = 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
 
 def runner
   prompt_user
-  selection(num)
+  selection(get_user_input)
 end
